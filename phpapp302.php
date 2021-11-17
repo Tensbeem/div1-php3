a68298ed298d71753aaa9744ebf29f397b0da934
Tensbeem <tensrrt1234@gmail.com>
2021-11-17 15:17:43 +0700

    Update

---
+++ phpapp302.php
@@ -0,0 +1,7 @@
+<?php
+$num = 12;
+if($num %2 == 0){
+    echo $num, " is True is even number";
+} else{
+    echo $num, " is False is odd number";
+}
---
+++ phpapp309.php
@@ -0,0 +1,17 @@
+<?php
+$isLogin = false;
+
+if($isLogin){
+    $title = 'Logout';
+}
+else{
+    $title = 'Login';
+}
+
+// echo $title;
+?>
+
+<?php
+$isLogin = false;
+$title = $isLogin? 'Logout':'Login';
+echo $title;
